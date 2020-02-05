01b835ebc1162df2bf806bad4fca6ee199f11152
Diego Vasquez <botellongo69@outlook.com>
2020-02-05 19:13:20 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,4 +7,12 @@ class BankAccount
     @balance = 1000
     @status = 'open'
   end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
 end
