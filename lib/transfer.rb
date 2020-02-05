e9287c434bdd95b5b6b991d2c5b97f60aa7e016f
oliverfeher <oliver92.feher@gmail.com>
2020-02-05 01:29:54 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
-  # your code here
+
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid?
+    @receiver.valid?
+  end
+
+  def execute_transaction
+    if @sender.balance < @amount
+      return "Transaction rejected. Please check your account balance"
+      self.status = "rejected"
+    else
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      self.status = "complete"
+    end
+  end
+
 end
