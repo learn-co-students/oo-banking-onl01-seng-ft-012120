f2a72510d89c092f220241b85a84af8e5f66d980
Diego Vasquez <botellongo69@outlook.com>
2020-02-05 19:32:09 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,4 +8,22 @@ class Transfer
     @status = 'pending'
     @amount = transfer_amount
   end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    if @status == 'pending' && @sender.balance >= @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status == 'complete'
+    else
+      'Transaction rejected. Please check your account balance.'
+    end
+  end
 end
