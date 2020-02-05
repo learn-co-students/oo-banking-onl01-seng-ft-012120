88386e3ace271db4e937ea00b26ef1856b0b38b3
Diego Vasquez <botellongo69@outlook.com>
2020-02-05 19:23:19 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
+  attr_reader :sender, :receiver, :status
+  attr_accessor :amount
   
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = transfer_amount
+  end
 end
