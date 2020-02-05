f083644e976c991ef2a554cc423481f095fd7731
Diego Vasquez <botellongo69@outlook.com>
2020-02-05 19:50:45 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,12 +18,17 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == 'pending' && @sender.balance >= @amount
+    if @status == 'pending' && self.valid? && sender.balance >= @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status == 'complete'
+      @status = 'complete'
     else
+      @status = 'rejected'
       'Transaction rejected. Please check your account balance.'
     end
+  end
+  
+  def reverse_transfer
+    
   end
 end
